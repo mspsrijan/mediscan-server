150a35da11aaf380e42fc23ebf947283cca6e394
Shah Paran Srijan <mspsrijan@gmail.com>
2023-11-26 20:50:15 +0600

    Configured JWT authnetication and GET, POST APIs for Jobs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -27,8 +28,33 @@ app.listen(port);
 
 async function run() {
   try {
-    client.connect();
+    // await client.connect();
 
+    // JWT Related API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // JWT Middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized Access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    //User Related APIs
     const usersCollection = client.db("JobVerse").collection("users");
 
     app.post("/users", async (req, res) => {
@@ -43,6 +69,27 @@ async function run() {
         const user = await usersCollection.insertOne(newUser);
         res.send(user);
       }
+    });
+
+    //Job Related APIs
+    const jobsCollection = client.db("JobVerse").collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/jobs", verifyToken, async (req, res) => {
+      const item = req.body;
+      const result = await jobsCollection.insertOne(item);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
